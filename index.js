cf2162d58a829a8ef5c7412770ffb229a6917477
arafataft <arafatcd@gmail.com>
2023-05-03 11:19:19 +0600

    load data on server root

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app =express();
+const cors= require('cors');
 const port = process.env.PORT || 3000;
 
+const chefData = require('./data/chefData.json')
+
 app.get('/',(req,res)=>{
-    res.send('Server Running')
+    res.send(chefData);
 });
 
 app.listen(port,()=>{
